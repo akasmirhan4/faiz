e4c3d8729188e402582ff4ef72ffbd9200d9e4c3
akasmirhan4 <amirrul.kasmirhan@gmail.com>
2021-04-13 00:17:49 +0800

    implement Enter and Backspace listener to add or remove line

--- js/main.js
+++ js/main.js
@@ -1,18 +1,64 @@
 let textArea = document.getElementById("textArea");
 
-const NLINES = 500;
-for (let i = 1; i <= NLINES; i++) {
+function updateLineNo(node, lineNo) {
+    var nextSibling = node.nextElementSibling;
+    while (nextSibling) {
+        nextSibling.setAttribute('line-no', lineNo)
+        nextSibling.children[0].innerText = lineNo;
+
+        lineNo++;
+        nextSibling = nextSibling.nextElementSibling;
+    }
+}
+
+function addNewLine(lineNo = 1, referenceNode = null) {
     let newDiv = document.createElement("div");
     let number = document.createElement("span");
     let text = document.createElement("span");
 
     newDiv.classList.add("editorLines");
-    newDiv.setAttribute("line-no", i);
-    number.innerText = i;
+
+
+    newDiv.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            // Prevent default newline /n
+            event.preventDefault();
+        }
+    });
+
+    newDiv.addEventListener("keyup", function (event) {
+        // Enter
+        if (event.key === "Enter") {
+            let lineNo = parseInt(this.getAttribute('line-No'));
+            addNewLine(lineNo + 1, this.nextElementSibling);
+            if (this.nextElementSibling) {
+                lineNo++;
+                updateLineNo(this, lineNo);
+            }
+            this.nextElementSibling.children[1].focus();
+        }
+        // Backspace
+        else if (event.key === "Backspace") {
+            if (!this.children[1].innerText) {
+                if (this.previousElementSibling) {
+                    let lineNo = parseInt(this.getAttribute('line-No'));
+                    this.previousElementSibling.children[1].focus();
+
+                    updateLineNo(this, lineNo);
+                    this.remove();
+                }
+            }
+        }
+    });
+
+    newDiv.setAttribute("line-no", lineNo);
+    number.innerText = lineNo;
     number.classList.add("nambah");
     text.classList.add("editables");
     text.setAttribute("contenteditable", "true");
     newDiv.append(number);
     newDiv.append(text);
+    textArea.insertBefore(newDiv, referenceNode);
     textArea.append(newDiv);
 }
+addNewLine();
